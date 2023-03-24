f4ec1d511776c23bf5117f79d1326a1bf9a5437f
Mohammad H <mhus3040@gmail.com>
2023-03-24 17:46:58 -0400

    new change

--- src/test/java/pages/Temp.java
+++ src/test/java/pages/Temp.java
@@ -3,5 +3,6 @@ package pages;
 public class Temp {
     public static void main(String[] args) {
         System.out.println("Testing role on github");
+        System.out.println("Testing role on github");
     }
 }
